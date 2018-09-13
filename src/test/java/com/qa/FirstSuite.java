fc3412d0b5f390dd2cbc821ae0b8b786d22acf59
Rajanish Bhagat <raj814154@gmail.com>
2018-09-13 21:38:57 +0530

    first commit

---
+++ src/test/java/com/qa/FirstSuite.java
@@ -0,0 +1,19 @@
+package com.qa;
+import org.testng.annotations.*;
+
+public class FirstSuite {
+	
+	
+	@Test
+	public void dispaly()
+	{
+		System.out.println("FirstTestcase");
+	}
+	
+	@Test
+	public void show()
+	{
+		System.out.println("SecondTestcase");
+	}
+
+}
---
+++ src/test/java/com/qa/SecondSuite.java
@@ -0,0 +1,20 @@
+package com.qa;
+
+import org.testng.annotations.Test;
+
+public class SecondSuite {
+
+
+	@Test
+	public void look()
+	{
+		System.out.println("FirstTestcase");
+	}
+	
+	@Test
+	public void see()
+	{
+		System.out.println("SecondTestcase");
+	}
+
+}
